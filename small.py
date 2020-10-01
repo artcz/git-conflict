b44a475ff2886df4600857df50cb26e126b96f94
Artur Czepiel <czepiel.artur@gmail.com>
2020-10-01 18:56:33 +0200

    added count3 to small.py

--- small.py
+++ small.py
@@ -1,10 +1,21 @@
 from sys import exit
+import itertools
 
 
 def count(start, end, interval):
     while start < end:
         yield start
         start += interval
+
+
+def count3(start, end, interval):
+    counter = itertools.count(start, interval)
+
+    for c in counter:
+        if c >= end:
+            break
+
+        yield c
 
 
 def main():
